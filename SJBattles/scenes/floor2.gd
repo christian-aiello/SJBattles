9b953fd857db133c75ff619545c556f910872801
GiuseppeCarito <105475584+GiuseppeCarito@users.noreply.github.com>
2022-06-16 23:59:32 -0400

    Randomized Cookie Positions

---
+++ SJBattles/scenes/floor2.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+var rng = RandomNumberGenerator.new()
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Cookie.position.x = rng.randi_range(-1551, 1551)
+	$Cookie.position.y = rng.randi_range(-145, -47)
